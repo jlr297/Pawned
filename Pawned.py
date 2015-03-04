277cd224fdbbd13e95134ff38e1f14cc21e48a3e
Jesse Rolheiser <jlr297@gmail.com>
2015-03-04 12:15:21 -0600

    Now supports Comp vs. Comp. Added in minimax with alpha-beta pruning

--- Pawned.py
+++ Pawned.py
@@ -71,6 +71,22 @@ class Board():
                 if piece.col == move.col and piece.row == move.row:
                     self.white.remove(piece)
 
+# Function to return a new board that is a copy of a given board
+def copy_board(board):
+    newBoard = Board()
+    newBoard.board = [["  " for x in range(board.col)] for x in range(board.row)]
+    newBoard.row = board.row
+    newBoard.col = board.col
+    newBoard.white = []
+    newBoard.black = []
+    for piece in board.white:
+        newBoard.white.append(Piece(piece.label, piece.row, piece.col))
+        newBoard.board[piece.row][piece.col] = piece.label
+    for piece in board.black:
+        newBoard.black.append(Piece(piece.label, piece.row, piece.col))
+        newBoard.board[piece.row][piece.col] = piece.label
+    return newBoard
+
 # Successor function, generates possible moves for a player
 #   board - The board state
 #   Wturn - Bool, whose turn it is
@@ -102,30 +118,47 @@ def Successor(board, Wturn):
 def terminal_board(board):
     # If white is out of pieces
     if len(board.white) == 0:
-        print "Black Wins!"
         return True
     # If black is out of pieces
     if len(board.black) == 0:
-        print "White Wins!"
         return True
     # If a white piece reaches the top row
     for piece in board.white:
         if piece.row == 0:
-            print "White Wins!"
             return True
     # If a black piece reaches the bottom row
     for piece in board.black:
         if piece.row == board.col:
-            print "Black Wins!"
             return True
     # If both players have no remaining moves
     white_moves = Successor(board, True)
     black_moves = Successor(board, False)
     if len(white_moves) == 0 and len(black_moves) == 0:
-        print "It's a draw!"
         return True
     # Else the board is not in a terminal state
     return False
+
+# Determine value for a given terminal board
+def terminal_val(board):
+    # If white is out of pieces
+    if len(board.white) == 0:
+        return -100000
+    # If black is out of pieces
+    if len(board.black) == 0:
+        return 100000
+    # If a white piece reaches the top row
+    for piece in board.white:
+        if piece.row == 0:
+            return 100000
+    # If a black piece reaches the bottom row
+    for piece in board.black:
+        if piece.row == board.col:
+            return -100000
+    # If both players have no remaining moves
+    white_moves = Successor(board, True)
+    black_moves = Successor(board, False)
+    if len(white_moves) == 0 and len(black_moves) == 0:
+        return 0
 
 # Helper function to determine if 2 moves are equal
 def moves_equal(move1, move2):
@@ -135,22 +168,87 @@ def moves_equal(move1, move2):
                 return True
     return False
 
-# Start of Main
-board = Board()
-board.print_board()
-
-white_turn = True
-play_game = True
-
-while play_game == True:
-    if white_turn:
-        print "White's Turn"
+# Function MiniMax to determine best possible move
+def minimax(state, maxing, depth = -1, h = None):
+    if terminal_board(state):
+        return (terminal_val(state), Move(""))
+    if depth == 0:
+        return (h(state), Move(""))
+    if maxing:
+        successor_moves = Successor(state, maxing)
+        bestMax = (-1000, Move(""))
+        if len(successor_moves) > 0:
+            bestMax = (-1000, Move(successor_moves[0].label, successor_moves[0].row, successor_moves[0].col))
+            for suc_move in successor_moves:
+                newBoard = copy_board(state)
+                newBoard.apply_move(suc_move)
+                val = minimax(newBoard, False, depth-1, h)
+                if val[0] > bestMax[0]:
+                    bestMax = (val[0], suc_move)
+        return bestMax
     else:
-        print "Black's Turn"
+        successor_moves = Successor(state, maxing)
+        bestMin = (1000, Move(""))
+        if len(successor_moves) > 0:
+            bestMin = (1000, Move(successor_moves[0].label, successor_moves[0].row, successor_moves[0].col))
+            for suc_move in successor_moves:
+                newBoard = copy_board(state)
+                newBoard.apply_move(suc_move)
+                val = minimax(newBoard, True, depth-1, h)
+                if val[0] < bestMin[0]:
+                    bestMin = (val[0], suc_move)
+        return bestMin
+
+# Function MiniMax to determine best possible move, with alph-beta pruning
+def minimax_ab(state, maxing, depth, h, alpha, beta):
+    if terminal_board(state):
+        return (terminal_val(state), Move(""))
+    if depth == 0:
+        return (h(state), Move(""))
+    if maxing:
+        successor_moves = Successor(state, maxing)
+        bestMax = (-1000, Move(""))
+        if len(successor_moves) > 0:
+            bestMax = (-1000, Move(successor_moves[0].label, successor_moves[0].row, successor_moves[0].col))
+            for suc_move in successor_moves:
+                newBoard = copy_board(state)
+                newBoard.apply_move(suc_move)
+                val = minimax_ab(newBoard, False, depth-1, h, alpha, beta)
+                if val[0] > bestMax[0]:
+                    bestMax = (val[0], suc_move)
+                alpha = max(alpha, val[0])
+                if beta <= alpha:
+                    break
+        return bestMax
+    else:
+        successor_moves = Successor(state, maxing)
+        bestMin = (1000, Move(""))
+        if len(successor_moves) > 0:
+            bestMin = (1000, Move(successor_moves[0].label, successor_moves[0].row, successor_moves[0].col))
+            for suc_move in successor_moves:
+                newBoard = copy_board(state)
+                newBoard.apply_move(suc_move)
+                val = minimax_ab(newBoard, True, depth-1, h, alpha, beta)
+                if val[0] < bestMin[0]:
+                    bestMin = (val[0], suc_move)
+                beta = min(beta, val[0])
+                if beta <= alpha:
+                    break
+        return bestMin
+   
+# Estimated value for who is winning the current game
+def eval_board(board): ## higher better for white, lower better for black
+    h_cost = 0
+    h_cost += (len(board.white) - len(board.black))
+    return h_cost
+
+# Gets input from player and makes the move if it's valid
+def player_turn(board, white_turn):
+    play_game = True
     inputs = raw_input("Enter a move, format \"piece row col\" Type 'help' for help: ").split()
     if len(inputs) == 1:
         if inputs[0] == 'q':
-            break
+            play_game = False
         elif inputs[0] == 'p':
             board.print_board()
         elif inputs[0] == 'm':
@@ -178,4 +276,50 @@ while play_game == True:
                     print "No possible moves, skipped Turn"
                     white_turn = not white_turn
                 break
+    return (white_turn, play_game)
+
+def comp_turn(board, white_turn, depth_limit):
+    # move = minimax(board, white_turn, depth_limit, eval_board)
+    move = minimax_ab(board, white_turn, depth_limit, eval_board, -100000, 100000)
+    print move[1].label + " " + str(move[1].row) + " " + str(move[1].col)
+    board.apply_move(move[1])
+    board.print_board()
+
+# Start of Main
+board = Board()
+board.print_board()
+
+white_turn = True
+play_game = True
+
+p1_cpu = True
+p2_cpu = True
+
+depth_limit = 12
+
+while play_game == True:
+    if white_turn:
+        print "White's Turn"
+    else:
+        print "Black's Turn"
+    if white_turn:
+        if p1_cpu:
+            comp_turn(board, white_turn, depth_limit)
+            white_turn = False
+        else:
+            turn = player_turn(board, white_turn)
+            white_turn = turn[0]
+            play_game = turn[1]
+        if terminal_board(board):
+            play_game = False
+    else:
+        if p2_cpu:
+            comp_turn(board, white_turn, depth_limit)
+            white_turn = True
+        else:
+            turn = player_turn(board, white_turn)
+            white_turn = turn[0]
+            play_game = turn[1]
+        if terminal_board(board):
+            play_game = False
 quit = input("Game Over")
